5bfd4d09856ea8f73cc9f27d0029c6ddff7b010f
Chris Peralta <chrisperalta1998@gmail.com>
2024-03-08 19:18:24 -0800

    added get and post request to get all books and add a book to db

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import { Book } from "./models/bookModel.js";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (request, response) => {
   console.log(request.status(234).send("welcome"));
 });
@@ -15,7 +17,35 @@ app.post("/books", async (request, response) => {
       !request.body.title ||
       !request.body.author ||
       !request.body.publishYear
-    );
+    ) {
+      return response.status(400).send({
+        message: "Send all required fields: title, author, publishYear",
+      });
+    }
+
+    const newBook = {
+      title: request.body.title,
+      author: request.body.author,
+      publishYear: request.body.publishYear,
+    };
+
+    const book = await Book.create(newBook);
+
+    return response.status(201).send(book);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+app.get("/books", async (request, response) => {
+  try {
+    const books = await Book.find({});
+
+    return response.status(200).json({
+      count: books.length,
+      data: books,
+    });
   } catch (error) {
     console.log(error.message);
     response.status(500).send({ message: error.message });
